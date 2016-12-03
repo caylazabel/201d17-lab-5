e2b15e326d2947f8c6b1b44c26eb3c4c68ebfd11
Gary Lundgren <builtbygl@gmail.com>
2016-12-02 17:01:29 -0800

    number 5 solved

--- app.js
+++ app.js
@@ -109,10 +109,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+    var productOfNum = multiply(testArray[0],testArray[1])[0];
+    console.log(productOfNum);
+    var totalProductOfNum = multiply(productOfNum,testArray[2])[0];
+    console.log(totalProductOfNum);
+    var message5 = 'The numbers ' + testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' have a product of ' + totalProductOfNum + '.'
+    console.log(message5);
+  return [productOfNum, message5];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray([2,3,4]);
+ testMultiplyArray([2,3,4]);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
